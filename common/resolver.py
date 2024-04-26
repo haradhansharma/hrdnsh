09711ec52e67c879328358e9ad4d097e0ce90db9
haradhansharma <haradhan.sharma@gmail.com>
2024-04-26 20:23:09 +0600

    first commit

--- common/resolver.py
+++ common/resolver.py
@@ -2,10 +2,15 @@ from django.core.cache import cache
 from django.contrib.sites.models import Site
 
 def check_host(host, request, **kwargs): 
-    host = cache.get("hrdnsh_host")
-    if host:
+    if host.startwith('www.'):
+        hosts = [host, host.replace('www.', '')]
+    else:
+        hosts = [host, 'www.'+host]
+
+    hosts = cache.get("valid_hostnames")
+    if hosts:
         return True
-    elif Site.objects.filter(domain=host).exists():
-        cache.set("hrdnsh_host", host, 3000)      
+    elif Site.objects.filter(domain__in=hosts).exists():
+        cache.set("valid_hostnames", hosts, 3000)      
         return True
     return False
