b5b326f9bd1a9ace4622034ccbee66794fc94f1e
haradhansharma <haradhan.sharma@gmail.com>
2024-05-02 17:28:11 +0600

    multitemplate

--- common/admin.py
+++ common/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.sites.models import Site
 
 admin.site.register(SiteProfile)
 admin.site.register(KeyQualification)
 admin.site.register(SiteTemplate)
+
+admin.site.unregister(Site)
+admin.site.register(Site)
 
 
 class WhatDidThereInline(admin.TabularInline): 
