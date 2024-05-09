819e83259502c19ca5fe00b7e449ac313bcb5c8a
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 01:52:24 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -9,10 +9,10 @@ class CustomFileCache(FileBasedCache):
         os.chmod(self._dir, 0o770)
         os.umask(old_umask)
         
-    def _list_cache_files(self):        
-        list_of_file = [os.path.join(self._dir, fname) for fname in glob.glob1(self._dir, "*%s" % self.cache_suffix)]
-        for f in list_of_file:     
-            os.chmod(f, 0o666)     
-        return list_of_file
+    # def _list_cache_files(self):        
+    #     list_of_file = [os.path.join(self._dir, fname) for fname in glob.glob1(self._dir, "*%s" % self.cache_suffix)]
+    #     for f in list_of_file:     
+    #         os.chmod(f, 0o666)     
+    #     return list_of_file
         
 
