7725832659e908488fd41cca26086efd30241b86
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 02:03:49 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -3,10 +3,10 @@ import glob
 from django.core.cache.backends.filebased import FileBasedCache
 
 class CustomFileCache(FileBasedCache):
-    def get(self, key, default=None, version=None):
+    def get(self, key, default=None, version=None):        
+        super().get(key, default, version)
         fname = self._key_to_file(key, version)
         os.chmod(fname, 0o666) 
-        super().get(key, default, version)
         
    
         
