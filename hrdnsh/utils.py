84f59743dc89fbaeb7900a14d9cc83d6a897f3e7
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 03:59:43 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -2,8 +2,7 @@ import os
 from django.utils.crypto import md5
 from django.core.cache.backends.filebased import FileBasedCache
 
-class CustomFileCache(FileBasedCache):    
-  
+class CustomFileCache(FileBasedCache):     
         
     pass
    
