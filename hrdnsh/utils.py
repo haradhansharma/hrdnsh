9d861b2823d144199f9a7849f9f231ab84f58bcd
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 04:05:58 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -1,10 +1,28 @@
 import os
-from django.utils.crypto import md5
+import tempfile
+from django.core.files.move import file_move_safe
 from django.core.cache.backends.filebased import FileBasedCache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
-class CustomFileCache(FileBasedCache):     
+class CustomFileCache(FileBasedCache):  
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        self._createdir()
+        fname = self._key_to_file(key, version)
+        os.chmod(fname, 0o666)
+        self._cull()
+        fd, tmp_path = tempfile.mkstemp(dir=self._dir)
+        renamed = False
+        try:
+            with open(fd, "wb") as f:
+                self._write_content(f, timeout, value)
+            file_move_safe(tmp_path, fname, allow_overwrite=True)
+            renamed = True
+        finally:
+            if not renamed:
+                os.remove(tmp_path)  
+  
         
-    pass
+  
    
         
 
