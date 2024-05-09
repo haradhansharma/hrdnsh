9dbb320300f731bf2bc1679111eb1afb30283908
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 02:02:00 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -3,17 +3,11 @@ import glob
 from django.core.cache.backends.filebased import FileBasedCache
 
 class CustomFileCache(FileBasedCache):
-    pass
-    # def _createdir(self):      
-    #     super()._createdir()  
-    #     old_umask = os.umask(0o000) 
-    #     os.chmod(self._dir, 0o770)
-    #     os.umask(old_umask)
+    def get(self, key, default=None, version=None):
+        fname = self._key_to_file(key, version)
+        os.chmod(fname, 0o666) 
+        super().get(key, default, version)
         
-    # def _list_cache_files(self):        
-    #     list_of_file = [os.path.join(self._dir, fname) for fname in glob.glob1(self._dir, "*%s" % self.cache_suffix)]
-    #     for f in list_of_file:     
-    #         os.chmod(f, 0o666)     
-    #     return list_of_file
+   
         
 
