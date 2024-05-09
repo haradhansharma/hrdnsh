b64d378b42f1d0c2e0372031aceea6d0c14d2bd6
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 02:12:58 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -2,10 +2,11 @@ import os
 import glob
 from django.core.cache.backends.filebased import FileBasedCache
 
-class CustomFileCache(FileBasedCache):
-    def _write_content(self, file, timeout, value):
-        os.chmod(file, 0o666)
-        super()._write_content(file, timeout, value)
+class CustomFileCache(FileBasedCache):        
+    def has_key(self, key, version=None):
+        fname = self._key_to_file(key, version)
+        os.chmod(fname, 0o666)
+        super().has_key(key, version)
    
         
 
