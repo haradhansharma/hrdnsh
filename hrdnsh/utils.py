40339bd746374b8e641f5b9ae0cf55677ed54da8
haradhansharma <haradhan.sharma@gmail.com>
2024-05-10 02:21:08 +0600

    filebasecacheper

--- hrdnsh/utils.py
+++ hrdnsh/utils.py
@@ -5,20 +5,7 @@ from django.core.cache.backends.filebased import FileBasedCache
 class CustomFileCache(FileBasedCache):    
   
         
-    def _key_to_file(self, key, version=None):
-        
-        key = self.make_and_validate_key(key, version=version)
-        file = os.path.join(
-            self._dir,
-            "".join(
-                [
-                    md5(key.encode(), usedforsecurity=False).hexdigest(),
-                    self.cache_suffix,
-                ]
-            ),
-        )
-        os.chmod(file, 0o666)
-        return file
+    pass
    
         
 
